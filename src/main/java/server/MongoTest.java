66eae146e85b472cd4bcc347ff8c0c0146068acd
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-11-11 12:13:29 +0530

    refactor: reorganize project folder structure

--- src/main/java/server/MongoTest.java
+++ src/main/java/server/MongoTest.java
@@ -11,7 +11,7 @@ public class MongoTest {
 
         MongoCollection<Document> collection = db.getCollection("team");
 
-        Document player = new Document("name", "hi network quiz")
+        Document player = new Document("name", "Parindya1")
                 .append("score", 10);
 
         collection.insertOne(player);
